446be7bdc7bef775d3cead624888088fa172efa4
Theo Weller <weller.theodor@gmail.com>
2019-05-04 18:57:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ class Polygon {
   get getCount() {
     return this.array.length
   }
-  get perimeter(){
+  get perimeter() {
     return this.array.reduce((a, b) => a + b, 0)
   }
 }
 
 class Triangle extends Polygon {
-  get isValid(){
-    let result = this.getCount === 3 ? true : false;
-    return result;
+  get isValid() {
+    return this.getCount === 3 ? true : false;
   }
 }
