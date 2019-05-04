e08c185b16f964b653c695fddabf29d997638244
Theo Weller <weller.theodor@gmail.com>
2019-05-04 18:53:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ class Polygon {
   constructor(array){
     this.array = array;
   }
-
-get getCount() {
-  return this.array.length
+  get getCount() {
+    return this.array.length
+  }
+  get perimeter(){
+    return this.array.reduce((a, b) => a + b, 0)
+  }
 }
 
-get perimeter(){
-  return this.array.reduce((a, b) => a + b, 0)
-}
-
-
+class Triangle extends Polygon {
+  get isValid(){
+    this.getCount() === 3 ? true : false;
+  }
 }
